62c1b14f0e0da04c0952865a74ede42b21ae864b
Dustin Dowell <dustindowell22@gmail.com>
2017-04-08 12:18:16 -0500

    Use startsWith and endsWith functions

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+function startsWith (value, start) {
+  return value.slice(0, start.length) === start
+}
+
+function endsWith (value, end) {
+  return value.slice(value.length - end.length) === end
+}
+
 module.exports = function (paths, ignores) {
   let result = []
-  let edge = String.prototype
+  let edge
 
   for (let i = 0, ignoresLength = ignores.length; i < ignoresLength; i++) {
     let ignore = ignores[i]
 
     if (ignore.lastIndexOf('.') > ignore.lastIndexOf('/')) {
-      edge = edge.endsWith
+      edge = endsWith
     } else {
-      edge = edge.startsWith
+      edge = startsWith
     }
 
     for (let j = 0, pathsLength = paths.length; j < pathsLength; j++) {
       let path = paths[j]
 
-      if (!edge.call(path, ignore)) {
+      if (!edge(path, ignore)) {
         result[result.length] = path
       }
     }
