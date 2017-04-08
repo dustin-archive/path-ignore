89b890ba2df12d99abc031ab2ff89f1000d46ccc
Dustin Dowell <dustindowell22@gmail.com>
2017-04-08 11:34:17 -0500

    For instead of for of

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ module.exports = function (paths, ignores) {
   let result = []
   let edge = String.prototype
 
-  for (let ignore of ignores) {
+  for (let i = 0, ignoresLength = ignores.length; i < ignoresLength; i++) {
+    let ignore = ignores[i]
+
     if (ignore.lastIndexOf('.') > ignore.lastIndexOf('/')) {
       edge = edge.endsWith
     } else {
       edge = edge.startsWith
     }
 
-    for (let path of paths) {
+    for (let j = 0, pathsLength = paths.length; j < pathsLength; j++) {
+      let path = paths[j]
+
       if (!edge.call(path, ignore)) {
         result[result.length] = path
       }
